c194a8bdcb013e792d3e45adb1560aa35609cf8c
saadhossain <mshsarkar@gmail.com>
2022-09-14 22:21:45 +0600

    Time spent at history card shown as integer(no decimal)

--- history.js
+++ history.js
@@ -32,7 +32,7 @@ function displayHistory() {
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
   <div class="history-data">
-    <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${Math.round(test.timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   </div>
   `;
